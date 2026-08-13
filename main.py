5ef22be09ae444d2a9edfec086b88c859675a844
samTheHacker <mobilvodoo@googlemail.com>
2026-08-13 23:47:17 +0100

    Add argparse for user_prompt

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import argparse  
 
 def main():
-    print("Hello from ai-agent-project!")
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
     
@@ -14,16 +14,25 @@ def main():
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,)
     
+    parser = argparse.ArgumentParser(description="Sam's Chat Bot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
     response = client.chat.completions.create(    
         model = "openrouter/free",
         messages = [
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": args.user_prompt,
             }
         ]
     )
     
+    if not response.usage:
+        raise RuntimeError("No response usage") 
+    
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
     print(response.choices[0].message.content)
     
 
