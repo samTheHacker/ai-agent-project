43141173481d9e6235aa6a19499a699ac7bcce76
samTheHacker <mobilvodoo@googlemail.com>
2026-08-17 18:32:23 +0100

    Add List Files logic to get_files_info. Update test_get_files_info.py.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,23 +5,29 @@ import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     
     try:
+        files_info: list[str] = []
+        wd_absoulte_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(wd_absoulte_path, directory))
+            
+
+        # Will be True or False as finds the longest sub-path shared by two paths
         
-        if not os.path.isdir(directory):
+        if os.path.commonpath([wd_absoulte_path, target_path]) != wd_absoulte_path:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            
+        if not os.path.isdir(target_path):
             return f'Error: "{directory}" is not a directory'
         
-        wd_absoulte_path = os.path.abspath(working_directory)
-        full_path = os.path.normpath(os.path.join(wd_absoulte_path, directory))
-        
-        # Will be True or False as finds the longest sub-path shared by two paths
-        valid_target_dir = os.path.commonpath([wd_absoulte_path, full_path]) == wd_absoulte_path
-        
-        print(valid_target_dir)
-        
-        if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-        return f'Success: "{directory}" is within the working directory'   
+        for filename in os.listdir(target_path):
+            filepath = os.path.join(target_path, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )      
+               
+        return "\n".join(files_info)
     
     except Exception as e:
-         return f"Error encountered: {e}" 
+         return f"Error: {e}" 
     
